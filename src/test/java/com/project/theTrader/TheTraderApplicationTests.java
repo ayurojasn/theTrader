0a1c98a1bf4b42aee7c48b7c96c82b2416910270
Juliana <rojasnpaula@javeriana.edu.co>
2021-09-19 14:14:59 -0500

    application initialized

---
+++ src/test/java/com/project/theTrader/TheTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.theTrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
